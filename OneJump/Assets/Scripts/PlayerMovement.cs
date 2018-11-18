5c32d7ad0fb74945663f6f70d11b2d0c9500859d
Filipe <filipe.garcia1997@gmail.com>
2018-11-18 14:28:30 -0200

    JumpGame
    Game Inicial

---
+++ OneJump/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float ForcaFrontal = 1000f;
+    public float ForcaLateral = 600f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        rb.AddForce(0, 0, ForcaFrontal * Time.deltaTime);
+
+        // Input.GetAxis("horizontal")
+        if (Input.GetKey("d"))
+        {
+
+            rb.AddForce(ForcaLateral * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+
+            rb.AddForce(-ForcaLateral * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+
+            FindObjectOfType<GameManager>().EndGame();
+
+        }
+
+    }
+
+}
