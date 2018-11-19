eac74bf7b29bfa88981953d5429bf27b00377f80
Filipe <filipe.garcia1997@gmail.com>
2018-11-19 04:37:05 -0200

    Jump fix

--- OneJump/Assets/Scripts/PlayerMovement.cs
+++ OneJump/Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,9 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody rb;
     public float ForcaFrontal = 1000f;
-    public float ForcaLateral = 600f;
+    public LayerMask groundLayers;
+    public float jumpForce = 7;
+
 
     // Update is called once per frame
     void FixedUpdate()
@@ -15,12 +17,10 @@ public class PlayerMovement : MonoBehaviour
 
         rb.AddForce(0, 0, ForcaFrontal * Time.deltaTime);
 
-        // Input.GetAxis("horizontal")
- 
-        if (Input.GetKey("w"))
-        {
 
-            rb.AddForce(0, 50 * Time.deltaTime, 0, ForceMode.VelocityChange);
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
 
         if (rb.position.y < -1f)
